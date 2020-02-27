dbd3d362b78f916f6e62c608bb0cb5df5f8a022d
JasonNelemans <jasonnelemans@hotmail.com>
2020-02-27 12:37:27 +0100

    Changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Switch, Route } from "react-router-dom";
 import React from 'react';
 import HomePage from './pages/HomePage';
-import DiscoverMoviesPage from './pages/DiscoverMoviesPage'
+import DiscoverMobiesPage from './pages/DiscoverMoviesPage'
 import AboutPage from './pages/AboutPage';
 import NavBar from './components/NavBar';
 
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <NavBar />
       <Switch>
-        <Route path="/discover" component={DiscoverMoviesPage} />
+        <Route path="/discover" component={DiscoverMobiesPage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/" component={HomePage} />
       </Switch>
--- src/pages/DiscoverMoviesPage.js
+++ src/pages/DiscoverMoviesPage.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
 
-export default function DiscoverMoviesPage() {
+export default function DiscoverMobiesPage() {
+  const [searchText, set_searchText] = useState("");
+  
+  const search = async () => {
+    console.log("TODO search movies for:", searchText);
+  };
+
   return (
-    <div className="DiscoverMoviesPage">
-      Discover Movies page
+    <div>
+      <h1>Discover some movies!</h1>
+      <p>
+        <input
+          value={searchText}
+          onChange={e => set_searchText(e.target.value)}
+        />
+        <button onClick={search}>Search</button>
+      </p>
     </div>
-  )
+  );
 }
